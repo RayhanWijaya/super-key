e71f04076f260e7654d30accc03c07e324d03fb4
RayhanWijaya <rayhansatria2410@gmail.com>
2022-06-20 22:01:33 +0700

    Added method list to greet function in main.cpp
    Added method list to greet function in main.cpp which has two methods:
    
    * alter (generates a jumbled password e.g. '5#17f&03kjf')
    * random (changes the form of each character in a given password e.g. 'pokemon_rocks' to 'p0k3mOn-r0cK5')

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 void greet() {
 	std::cout << "super-key \n";
+
+	std::cout << '\n';
+
+	std::cout <<
+		"methods: \n"
+		"* random (generates a jumbled password e.g. '5#17f&03kjf') \n"
+		"* alter (changes the form of each character in a given password e.g. 'pokemon_rocks' to 'p0k3mOn-r0cK5') \n";
+
+	std::cout << '\n';
 }
 
 int main() {
